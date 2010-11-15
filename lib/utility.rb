6b6e6cab774336ec490449567069911a7e442813
Darragh Jones <darragh.jones@gmail.com>
2010-11-15 01:42:08 +0000

    use temp for disk cache

--- lib/utility.rb
+++ lib/utility.rb
@@ -1,10 +1,9 @@
 module Utility
 
  def open_url(url)
-    return open(url) { |u| u.read } # heroku has a read-only filesystem :(
     #logger.log(url)
     digest = Digest::MD5.hexdigest(url)
-    filename = 'db/html/' + digest
+    filename = "#{RAILS_ROOT}/tmp/html/" + digest
     if File.exists?(filename)
        s = File.open(filename, 'r') { |f| f.read }
     else
