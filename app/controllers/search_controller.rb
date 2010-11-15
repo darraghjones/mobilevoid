51109c78758a29d9b6942fa14836ef9a3f7b4868
Darragh Jones <darragh.jones@gmail.com>
2010-11-15 02:08:38 +0000

    search stuff

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,33 +1,36 @@
 
 
 class SearchController < ApplicationController
-  def index
-  end
+def index
+end
 
-  def new
-    if params[:s] || params[:ar] || params[:a] 
-      doc = Nokogiri::HTML(open_url("http://www.legalsounds.com/powerSearch?s=#{params[:s]}&ar=#{CGI.escape(params[:ar])}&a=#{params[:a]}"))
-	
-      @songs = Array.new
-      doc.css('table.tbllist tr.off').each do |n| 
-	      ar = Artist.new
-	      al = Artist.new
-	      s = Song.new
-	      s.Artist = ar
-	      s.Album = al
-	      s.Name = n.css('td')[1].text()
-	      s.Artist.Name = n.css('td')[2].text()  
-	      s.Album.Name = n.css('td')[3].text() 
+def new
+if params[:s] || params[:ar] || params[:a] 
+doc = Nokogiri::HTML(open_url("http://www.legalsounds.com/powerSearch?s=#{params[:s]}&ar=#{CGI.escape(params[:ar])}&a=#{params[:a]}"))
 
-	      @songs << s
-      end
-      respond_to do |format|
-         format.html { render :show }
-         format.js { render :show }
-      end
-    end
-  end
+@songs = Array.new
+doc.css('table.tbllist tr.off').each do |n| 
+ar = Artist.new
+al = Artist.new
+s = Song.new
+s.Artist = ar
+s.Album = al
+s.Name = n.css('td')[1].text()
+s.Url = n.css('div.preview div')[0]['href']
+s.Artist.Name = n.css('td')[2].text()  
+s.Artist.Url = n.css('td')[2].at_css('a') && n.css('td')[2].at_css('a')['href'] || ""
+s.Album.Name = n.css('td')[3].text()
+s.Album.Url = n.css('td')[3].at_css('a')['href']
 
-  
+@songs << s
+end
+respond_to do |format|
+format.html { render :show }
+format.js { render :show }
+end
+end
+end
+
+
 
 end
